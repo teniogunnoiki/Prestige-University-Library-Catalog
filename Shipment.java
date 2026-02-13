7da0a9a24f588586f2ddbf452f14dc7380400ac8
teniogunnoiki <victoriaog19@gmail.com>
2026-02-13 12:31:19 -0600

    implemented add method to add to shipment, and notification of delivery of shipment

--- Shipment.java
+++ Shipment.java
@@ -1,18 +1,28 @@
 import java.util.Date;
+import java.util.ArrayList;
 public class Shipment {
     private int shipmentID;
     private Date shipmentDate;
     private String status;
+    private ArrayList<LibraryItem> items;
 
     public Shipment(){
         this.shipmentID = 0;
         this.status = "Pending";
         this.shipmentDate = new Date();
+        this.items = new ArrayList<>();
     }
     public Shipment(int shipmentID, String status) {
         this.shipmentID = shipmentID;
         this.shipmentDate = new Date();
         this.status = status;
+        this.items = new ArrayList<>();
+    }
+    public void addItem(LibraryItem item) {
+        items.add(item);
+    }
+    public void deliverShipment() {
+        this.status = "Delivered";
     }
 
     public int getShipmentID() {
@@ -32,6 +42,7 @@ public class Shipment {
     public String toString() {
         return "Shipment ID: " + shipmentID +
                 ", Date: " + shipmentDate +
-                ", Status: " + status;
+                ", Status: " + status +
+                ", Items in Shipment: " + items.size();
     }
 }
