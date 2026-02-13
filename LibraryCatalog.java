e907072bf7532d47f53258973d57f710072a2e02
teniogunnoiki <victoriaog19@gmail.com>
2026-02-13 12:46:03 -0600

    created displayShipment and addShipment method

--- LibraryCatalog.java
+++ LibraryCatalog.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 import java.util.Collections;
-
 public class LibraryCatalog<T extends LibraryItem> {
     private String librarySession;
     private ArrayList<T> items = new ArrayList<>();
+    private ArrayList<Shipment> shipments = new ArrayList<>();
 
     public LibraryCatalog(String librarySession) {
         this.librarySession = librarySession;
@@ -22,14 +22,22 @@ public class LibraryCatalog<T extends LibraryItem> {
         }
     }
     public void findandRemoveItem(int item){
-    boolean found = items.removeIf(s1 -> s1.getItemID() == item);
-    if(found) {
-        System.out.println("Item Removed!");
-    } else {
-        System.out.println("Item not found.");
+        boolean found = items.removeIf(s1 -> s1.getItemID() == item);
+        if(found) {
+            System.out.println("Item Removed!");
+        } else {
+            System.out.println("Item not found.");
+        }
     }
-}
     public void sortLibrary(){
         Collections.sort(items);
+    }
+    public void addShipment(Shipment shipment){
+        shipments.add(shipment);
+    }
+    public void displayShipments(){
+        for(Shipment s : shipments){
+            System.out.println(s);
+        }
     }
 }
