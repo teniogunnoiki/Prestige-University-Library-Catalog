daeeed1381927608150c8f51ab3195f482390b12
teniogunnoiki <victoriaog19@gmail.com>
2026-02-19 17:04:26 -0600

    Update LibraryCatalog.java

--- LibraryCatalog.java
+++ LibraryCatalog.java
@@ -4,6 +4,8 @@ public class LibraryCatalog<T extends LibraryItem> {
     private String librarySession;
     private ArrayList<T> items = new ArrayList<>();
     private ArrayList<Shipment> shipments = new ArrayList<>();
+    private ArrayList<Warehouse> warehouses = new ArrayList<>();
+
 
     public LibraryCatalog(String librarySession) {
         this.librarySession = librarySession;
@@ -12,7 +14,8 @@ public class LibraryCatalog<T extends LibraryItem> {
     public void addItem(T item) {
         items.add(item);
     }
-    public int size(){
+
+    public int size() {
         return items.size();
     }
 
@@ -21,23 +24,58 @@ public class LibraryCatalog<T extends LibraryItem> {
             System.out.println(item.getDetails());
         }
     }
-    public void findandRemoveItem(int item){
+
+    public void findAndRemoveItem(int item) {
         boolean found = items.removeIf(s1 -> s1.getItemID() == item);
-        if(found) {
+        if (found) {
             System.out.println("Item Removed!");
         } else {
             System.out.println("Item not found.");
         }
     }
-    public void sortLibrary(){
+
+    public void sortLibrary() {
         Collections.sort(items);
     }
-    public void addShipment(Shipment shipment){
-        shipments.add(shipment);
-    }
-    public void displayShipments(){
-        for(Shipment s : shipments){
+
+    public void displayShipments() {
+        for (Shipment s : shipments) {
             System.out.println(s);
         }
+    }
+
+    public void createShipment(Shipment shipment) {
+        if (shipment == null) {
+            System.out.println("Cannot add null shipment!");
+            return;
+        }
+        shipments.add(shipment);
+        System.out.println("Shipment " + shipment.getShipmentID() + " added to catalog.");
+    }
+
+    public Shipment findShipment(int shipmentID) {
+        for (Shipment s : shipments) {
+            if (s.getShipmentID() == shipmentID) {
+                return s;
+            }
+        }
+        return null;
+    }
+
+    public void addItemToShipment(int itemID, int shipmentID) {
+        Shipment shipment = findShipment(shipmentID);
+        if (shipment == null) {
+            System.out.println("Shipment not found.");
+            return;
+        }
+        for (int i=0;i<items.size();i++) {
+            if (items.get(i).getItemID() == itemID) {
+                shipment.addItem(items.get(i));
+                items.remove(i);
+                System.out.println("Item moved to shipment.");
+                return;
+            }
+        }
+        System.out.println("Item not found in library.");
     }
 }
