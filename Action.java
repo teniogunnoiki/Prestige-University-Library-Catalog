f2c0a8c8fbeffdd918d2f7378b9032d2251cadf0
teniogunnoiki <victoriaog19@gmail.com>
2026-03-28 00:06:56 -0500

    Update Action.java to have setters and getters

--- Action.java
+++ Action.java
@@ -1,10 +1,25 @@
 public class Action {
-    String type;
-    Object data;
+    private String type;
+    private Object data;
 
     public Action(String type, Object data){
         this.type = type;
         this.data = data;
     }
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
 }
